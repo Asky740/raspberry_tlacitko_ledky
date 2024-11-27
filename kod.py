e0db60746618b12bf5a3da4e7696463a9ea451e1
Adam Škývara <Adam.Skyvara@student.souepl.cz>
2024-11-27 09:29:30 +0100

    knjbadfgue

---
+++ kod.py
@@ -0,0 +1,24 @@
+from machine import Pin
+from time import sleep
+
+led1 = Pin(0, Pin.OUT)
+led2 = Pin(1, Pin.OUT)
+led3 = Pin(2, Pin.OUT)
+led4 = Pin(3, Pin.OUT)
+
+button = Pin(4, Pin.OUT)
+
+while True:
+    sleep(0.1)
+    try:
+        print("------------------") 
+        if  :
+
+
+
+
+
+
+
+    except OSError as e:
+        print('Failed')
