78b738c905125e4b6ac9a21438993318fc5488e6
Adam Škývara <Adam.Skyvara@student.souepl.cz>
2024-12-10 13:42:39 +0100

    asdaasd

--- kod.py
+++ kod.py
@@ -4,21 +4,20 @@ from time import sleep
 led1 = Pin(0, Pin.OUT)
 led2 = Pin(1, Pin.OUT)
 led3 = Pin(2, Pin.OUT)
-led4 = Pin(3, Pin.OUT)
-
-button = Pin(4, Pin.OUT)
+button = Pin(3, Pin.IN, Pin.PULL_UP)
 
 while True:
-    sleep(0.1)
-    try:
-        print("------------------") 
-        if  :
-
-
-
-
-
-
-
-    except OSError as e:
-        print('Failed')
+    if button.value() == 0:
+        led1.on()
+        sleep(0.1)
+        led2.on()
+        sleep(0.1)
+        led3.on()
+        sleep(0.1)
+    else:
+        led3.off()
+        sleep(0.1)
+        led2.off()
+        sleep(0.1)
+        led1.off()
+        sleep(0.1)
